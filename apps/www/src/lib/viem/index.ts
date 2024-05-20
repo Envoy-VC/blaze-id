6b5d87d070df8352a843f64b626c4400d207a059
Vedant Chainani <vedantchainani1084@gmail.com>
2024-05-20 22:22:49 +0530

    format

--- apps/www/src/lib/viem/index.ts
+++ apps/www/src/lib/viem/index.ts
@@ -18,7 +18,6 @@ const metadata = {
   icons: ['https://avatars.githubusercontent.com/u/37784886'],
 };
 
-
 export const wagmiConfig: Config = createConfig({
   chains: [mainnet, sepolia, anvil],
   ssr: true,

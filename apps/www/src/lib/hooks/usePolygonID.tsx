a3cfd1a1a5e847b2af1f9e65769d014778407eb3
Vedant Chainani <vedantchainani1084@gmail.com>
2024-05-22 01:51:43 +0530

    chore: remove secret box

--- apps/www/src/lib/hooks/usePolygonID.tsx
+++ apps/www/src/lib/hooks/usePolygonID.tsx
@@ -1,33 +1,9 @@
-import React, { useState } from 'react';
+import { CredentialStatusType, core } from '@0xpolygonid/js-sdk';
 
-import {
-  BjjProvider,
-  CredentialStatusResolverRegistry,
-  CredentialStatusType,
-  CredentialWallet,
-  IdentityWallet,
-  IssuerResolver,
-  KMS,
-  KmsKeyType,
-  RHSResolver,
-  core,
-} from '@0xpolygonid/js-sdk';
-import { SecretBox } from '@veramo/kms-local';
-import { useLocalStorage } from 'usehooks-ts';
-
-import { dataStorage } from '../polygon-id';
-import { PolygonIDPrivateKeyStore } from '../storage';
+import { credentialWallet, identityWallet } from '../polygon-id';
 
 const usePolygonID = () => {
-  const [kmsSecretKey] = useLocalStorage<string | null>('KMS_SECRET_KEY', null);
-  const [identityWallet, setIdentityWallet] = useState<IdentityWallet | null>(
-    null
-  );
-  const [credentialWallet, setCredentialWallet] =
-    useState<CredentialWallet | null>(null);
-
   const createDID = async () => {
-    if (!identityWallet) return;
     const { did, credential } = await identityWallet.createIdentity({
       method: core.DidMethod.PolygonId,
       blockchain: core.Blockchain.Polygon,
@@ -40,33 +16,6 @@ const usePolygonID = () => {
 
     return { did, credential };
   };
-
-  const initialize = (kmsKey: string) => {
-    const memoryKeyStore = new PolygonIDPrivateKeyStore(new SecretBox(kmsKey));
-    const bjjProvider = new BjjProvider(KmsKeyType.BabyJubJub, memoryKeyStore);
-    const kms = new KMS();
-    kms.registerKeyProvider(KmsKeyType.BabyJubJub, bjjProvider);
-
-    const statusRegistry = new CredentialStatusResolverRegistry();
-    statusRegistry.register(
-      CredentialStatusType.SparseMerkleTreeProof,
-      new IssuerResolver()
-    );
-    statusRegistry.register(
-      CredentialStatusType.Iden3ReverseSparseMerkleTreeProof,
-      new RHSResolver(dataStorage.states)
-    );
-    const credWallet = new CredentialWallet(dataStorage, statusRegistry);
-    const wallet = new IdentityWallet(kms, dataStorage, credWallet);
-    setIdentityWallet(wallet);
-    setCredentialWallet(credWallet);
-  };
-
-  React.useEffect(() => {
-    if (kmsSecretKey) {
-      initialize(kmsSecretKey);
-    }
-  }, [kmsSecretKey]);
 
   return {
     identityWallet,
--- apps/www/src/lib/polygon-id/index.ts
+++ apps/www/src/lib/polygon-id/index.ts
@@ -1,11 +1,21 @@
 import {
+  BjjProvider,
+  CredentialStatusResolverRegistry,
+  CredentialStatusType,
   CredentialStorage,
+  CredentialWallet,
   EthStateStorage,
   type Identity,
   IdentityStorage,
+  IdentityWallet,
   IndexedDBDataSource,
+  IndexedDBPrivateKeyStore,
+  IssuerResolver,
+  KMS,
+  KmsKeyType,
   MerkleTreeIndexedDBStorage,
   type Profile,
+  RHSResolver,
   W3CCredential,
   defaultEthConnectionConfig,
 } from '@0xpolygonid/js-sdk';
@@ -21,3 +31,27 @@ export const dataStorage = {
   mt: new MerkleTreeIndexedDBStorage(40),
   states: new EthStateStorage(defaultEthConnectionConfig),
 };
+
+const memoryKeyStore = new IndexedDBPrivateKeyStore();
+const bjjProvider = new BjjProvider(KmsKeyType.BabyJubJub, memoryKeyStore);
+const kms = new KMS();
+kms.registerKeyProvider(KmsKeyType.BabyJubJub, bjjProvider);
+
+const statusRegistry = new CredentialStatusResolverRegistry();
+statusRegistry.register(
+  CredentialStatusType.SparseMerkleTreeProof,
+  new IssuerResolver()
+);
+statusRegistry.register(
+  CredentialStatusType.Iden3ReverseSparseMerkleTreeProof,
+  new RHSResolver(dataStorage.states)
+);
+export const credentialWallet = new CredentialWallet(
+  dataStorage,
+  statusRegistry
+);
+export const identityWallet = new IdentityWallet(
+  kms,
+  dataStorage,
+  credentialWallet
+);

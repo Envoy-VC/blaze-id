c7c8b3240ff83be1371eb9f9635a97f50f99dcf1
Vedant Chainani <vedantchainani1084@gmail.com>
2024-05-22 05:21:57 +0530

    chore: use dynamic components

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,85 +1,15 @@
 'use client';
 
-import React from 'react';
+import dynamic from 'next/dynamic';
 
-import { useLitAuth, usePolygonID, useVeramo } from '~/lib/hooks';
-
-import { Navbar } from '~/components';
-
-import { Button } from '~/components/ui/button';
+const DynamicHome = dynamic(() => import('../components/Home'), {
+  ssr: false,
+});
 
 const Home = () => {
-  const { authWithPasskey } = useLitAuth();
-  const { createDID, createCredential } = useVeramo();
-  const { createDID: createPolygonIDDID } = usePolygonID();
-
   return (
     <div>
-      <Navbar />
-      <div className='flex w-fit flex-col gap-2'>
-        start by mint pkp
-        <Button
-          onClick={async () => {
-            // const res = await mintPKP('Vedant');
-            // console.log(res);
-          }}
-        >
-          Mint PKP
-        </Button>
-        <Button
-          onClick={async () => {
-            const res = await createDID({
-              provider: 'did:key',
-            });
-            console.log(res);
-          }}
-        >
-          Generate DID
-        </Button>
-        <Button
-          onClick={async () => {
-            const res = await createCredential({
-              credential: {
-                issuer: {
-                  id: 'did:key:z6Mkvi4DjUA2MTReKLYXMqaumUYVfXWuwbC8uXjNEZkhqh3H',
-                },
-                credentialSubject: {
-                  id: 'did:key:z6Mkvi4DjUA2MTReKLYXMqaumUYVfXWuwbC8uXjNEZkhqh3H',
-                  you: 'Rock',
-                },
-              },
-              proofFormat: 'jwt',
-            });
-            console.log(res);
-          }}
-        >
-          Create Credential
-        </Button>
-        <Button
-          onClick={async () => {
-            // const res = await mintPKP('Vedant');
-            // console.log(res);
-          }}
-        >
-          Mint PKP
-        </Button>
-        <Button
-          onClick={async () => {
-            const res = await createPolygonIDDID();
-            console.log(res);
-          }}
-        >
-          Create Polygon DID
-        </Button>
-        <Button
-          onClick={async () => {
-            const res = await authWithPasskey();
-            console.log(res);
-          }}
-        >
-          Auth With Passkey
-        </Button>
-      </div>
+      <DynamicHome />
     </div>
   );
 };
---
+++ apps/www/src/components/Home.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import { useLitAuth, usePolygonID, useVeramo } from '~/lib/hooks';
+
+import Navbar from './navbar';
+import { Button } from './ui/button';
+
+const HomeComponent = () => {
+  const { authWithPasskey } = useLitAuth();
+  const { createDID, createCredential } = useVeramo();
+  const { createDID: createPolygonIDDID } = usePolygonID();
+
+  return (
+    <div>
+      <Navbar />
+      <div className='flex w-fit flex-col gap-2'>
+        start by mint pkp
+        <Button
+          onClick={async () => {
+            // const res = await mintPKP('Vedant');
+            // console.log(res);
+          }}
+        >
+          Mint PKP
+        </Button>
+        <Button
+          onClick={async () => {
+            const res = await createDID({
+              provider: 'did:key',
+            });
+            console.log(res);
+          }}
+        >
+          Generate DID
+        </Button>
+        <Button
+          onClick={async () => {
+            const res = await createCredential({
+              credential: {
+                issuer: {
+                  id: 'did:key:z6Mkvi4DjUA2MTReKLYXMqaumUYVfXWuwbC8uXjNEZkhqh3H',
+                },
+                credentialSubject: {
+                  id: 'did:key:z6Mkvi4DjUA2MTReKLYXMqaumUYVfXWuwbC8uXjNEZkhqh3H',
+                  you: 'Rock',
+                },
+              },
+              proofFormat: 'jwt',
+            });
+            console.log(res);
+          }}
+        >
+          Create Credential
+        </Button>
+        <Button
+          onClick={async () => {
+            // const res = await mintPKP('Vedant');
+            // console.log(res);
+          }}
+        >
+          Mint PKP
+        </Button>
+        <Button
+          onClick={async () => {
+            const res = await createPolygonIDDID();
+            console.log(res);
+          }}
+        >
+          Create Polygon DID
+        </Button>
+        <Button
+          onClick={async () => {
+            const res = await authWithPasskey();
+            console.log(res);
+          }}
+        >
+          Auth With Passkey
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeComponent;

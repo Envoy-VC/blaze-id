d047f5f1f030f212ffb643ba2175340daffa9578
Vedant Chainani <vedantchainani1084@gmail.com>
2024-05-24 15:49:12 +0530

    chore: add shiki and credential proof

--- apps/www/src/app/dashboard/credentials/components/CredentialDetails.tsx
+++ apps/www/src/app/dashboard/credentials/components/CredentialDetails.tsx
@@ -1,17 +1,21 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import QRCode from 'react-qr-code';
 
 import { getKYCAgeProofRequest } from '~/lib/credentials';
 import { usePolygonID, useVeramo } from '~/lib/hooks';
 import { useBlazeStore } from '~/lib/stores';
+import { truncate } from '~/lib/utils';
 
 import { W3CCredential } from '@0xpolygonid/js-sdk';
 import { useLiveQuery } from 'dexie-react-hooks';
+import { getHighlighterCore } from 'shiki/core';
+import getWasm from 'shiki/wasm';
 import { toast } from 'sonner';
 
 import { Button } from '~/components/ui/button';
+import { ScrollArea } from '~/components/ui/scroll-area';
 
 import { Copy, ShieldCheck } from 'lucide-react';
 
@@ -20,14 +24,26 @@ interface Props {
   type: 'polygonid' | 'veramo';
 }
 
-const truncate = (value: string, len: number): string => {
-  return value.length > len ? value.substr(0, len - 1) + '...' : value;
-};
-
 const CredentialDetails = ({ id, type }: Props) => {
   const { getCredentialByHash } = useVeramo();
   const { getCredential, verifyCredential } = usePolygonID();
   const { activeDID } = useBlazeStore();
+
+  const [verifiedProof, setProof] = useState<string | null>(null);
+
+  const getFormattedCode = async (value: string) => {
+    const highlighter = await getHighlighterCore({
+      langs: [import('shiki/langs/json.mjs')],
+      loadWasm: getWasm,
+    });
+    await highlighter.loadTheme(import('shiki/themes/catppuccin-latte.mjs'));
+
+    const code = highlighter.codeToHtml(value, {
+      lang: 'json',
+      theme: 'catppuccin-latte',
+    });
+    return code;
+  };
 
   const credential = useLiveQuery(async () => {
     if (type === 'polygonid') {
@@ -50,10 +66,11 @@ const CredentialDetails = ({ id, type }: Props) => {
     const proofType = (proof as object[] as { type: string }[])[0]!.type;
     return (
       <div className='mx-auto my-12 flex w-full max-w-2xl flex-col gap-2 rounded-2xl bg-white'>
-        <div className='flex w-full flex-row items-center justify-between gap-2 px-4 py-2'>
+        <div className='flex w-full flex-row items-center justify-between gap-2 px-4 py-4'>
           <div className='text-xl font-semibold'>{type[1]}</div>
           <Button
             variant='outline'
+            className='flex items-center gap-2'
             onClick={async () => {
               const id = toast.loading('Verifying Credentials...');
               try {
@@ -69,7 +86,10 @@ const CredentialDetails = ({ id, type }: Props) => {
                 if (!success) {
                   throw new Error('Credential Verification Failed');
                 }
-                console.log(proof, success);
+                const code = await getFormattedCode(
+                  JSON.stringify(proof, null, 2)
+                );
+                setProof(code);
                 toast.success('Credentials Verified', { id });
               } catch (error) {
                 console.log(error);
@@ -101,7 +121,7 @@ const CredentialDetails = ({ id, type }: Props) => {
           <div className='flex flex-row items-center justify-between gap-2'>
             <div className='font-medium text-neutral-400'>Issuer</div>
             <div className='flex flex-row items-center gap-2'>
-              {truncate(issuer, 36)}
+              {truncate(issuer, 36, false)}
               <Button
                 variant='link'
                 className='m-0 h-4 w-4 p-0'
@@ -121,7 +141,7 @@ const CredentialDetails = ({ id, type }: Props) => {
               <div className='flex flex-row items-center justify-between gap-2'>
                 <div className='font-medium text-neutral-400'>{key}</div>
                 <div className='flex flex-row items-center gap-2'>
-                  {truncate(String(value), 36)}
+                  {truncate(String(value), 36, false)}
                   {String(value).length > 36 && (
                     <Button
                       variant='link'
@@ -141,6 +161,17 @@ const CredentialDetails = ({ id, type }: Props) => {
         <div className='mx-auto py-4'>
           <QRCode value={JSON.stringify(credential)} size={256 * 2} />
         </div>
+        {verifiedProof && (
+          <div className='w-full py-2'>
+            <div className='py-4 text-lg font-medium px-4'>Credential Proof</div>
+            <div
+              className='mx-auto h-[40rem] w-full max-w-[630px] overflow-scroll rounded-xl bg-[#EFF1F5] p-2'
+              dangerouslySetInnerHTML={{
+                __html: verifiedProof,
+              }}
+            ></div>
+          </div>
+        )}
       </div>
     );
   } else if (!!credential) {
@@ -181,7 +212,7 @@ const CredentialDetails = ({ id, type }: Props) => {
           <div className='flex flex-row items-center justify-between gap-2'>
             <div className='font-medium text-neutral-400'>Issuer</div>
             <div className='flex flex-row items-center gap-2'>
-              {truncate(issuer, 36)}
+              {truncate(issuer, 36, false)}
               <Button
                 variant='link'
                 className='m-0 h-4 w-4 p-0'
@@ -201,7 +232,7 @@ const CredentialDetails = ({ id, type }: Props) => {
               <div className='flex flex-row items-center justify-between gap-2'>
                 <div className='font-medium text-neutral-400'>{key}</div>
                 <div className='flex flex-row items-center gap-2'>
-                  {truncate(String(value), 36)}
+                  {truncate(String(value), 36, false)}
                   {String(value).length > 36 && (
                     <Button
                       variant='link'
